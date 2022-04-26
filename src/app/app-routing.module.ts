cbe96a95f5c05620565439399795979dcd403b72
ANTOINE BOULET <antoine.boulet@worldline.com>
2022-04-26 19:24:14 +0200

    delete compontent game + add routing for risky

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
-import {AppComponent} from "./app.component";
-import {GameComponent} from "./game/game.component";
 import {HomeComponent} from "./home/home.component";
 import {HistoryComponent} from "./history/history.component";
+import {RiskyComponent} from "./risky/risky.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'game', component: GameComponent},
+  {path: 'game', component: RiskyComponent},
   {path: 'history', component: HistoryComponent},
 
 
